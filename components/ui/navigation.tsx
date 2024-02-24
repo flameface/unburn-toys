2292fdc17cea30f807eb88e5f34ab4990b081ebd
FlameFace <kandepatilkunal@gmail.com>
2024-02-24 12:05:14 +0530

    init

--- components/ui/navigation.tsx
+++ components/ui/navigation.tsx
@@ -6,10 +6,11 @@ export function Navigation() {
             <a href="/">
                 <img className="logo" src="./logo.svg" alt="Logo" draggable="false" />
             </a>
-            <a href="https://github.com/flameface/unburn-toys"></a>
-            <div className="button">
-                <img src="/github.svg"/>
-            </div>
+            <a href="https://github.com/flameface/unburn-toys">
+                <div className="button">
+                    <img src="/github.svg" />
+                </div>
+            </a>
         </nav>
     )
 }
